0b17f20591c2b6182f4831d230a6fca1c146f2e0
Hanzel <hurrutia@lafise.com>
2024-08-24 11:45:25 -0600

    save: save other clientId

--- auth0-configuration.js
+++ auth0-configuration.js
@@ -1,5 +1,5 @@
 const config = {
-  clientId: "iA5j35BeTgL7Z14DkgJG34IHW95ykljM",
+  clientId: "iA5j35BeTgL7Z14DkgJG34IHW95ykljM", //clientId: "8gEAjvFx5zKWHBMRap7GCFXNuofrLVU4",
   domain: "lafise.cic-demo-platform.auth0app.com"
 };
 
